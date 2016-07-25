f2d7bec924874d33ab8d8433e5e3e99d8c431af1
Timothy Miller <tmiller@sailab.com>
2016-07-25 16:26:53 -0400

    Completed GeoQuiz. The challenge portion is not yet completed.

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -12,7 +12,15 @@ public class QuizActivity extends AppCompatActivity {
     private TextView mQuestionView;
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private int mCurrentIndex;
 
+    private Question[] mQuestionBank = new Question[] {
+        new Question(R.string.question_oceans, true),
+        new Question(R.string.question_americas, true),
+        new Question(R.string.question_asia, true),
+        new Question(R.string.question_mideast, false)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,27 +29,58 @@ public class QuizActivity extends AppCompatActivity {
 
         wireWidgets();
         setListeners();
+
+        updateQuestion();
     }
 
-    // get references to
+    // get references to view objects.
     private void wireWidgets() {
         mQuestionView = (TextView) findViewById(R.id.questionTextView);
         mTrueButton   = (Button)   findViewById(R.id.trueButton);
         mFalseButton  = (Button)   findViewById(R.id.falseButton);
+        mNextButton   = (Button)   findViewById(R.id.nextButton);
+        mCurrentIndex = 0;
+
     }
 
+    // set listeners to view objects.
     private void setListeners() {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
 
         mFalseButton.setOnClickListener((new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
+            }
+        }));
+
+        mNextButton.setOnClickListener((new View.OnClickListener() {
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
             }
         }));
     }
 
+    // update the view with the latest question.
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionView.setText(question);
+    }
+
+    // check the answer to make sure it's correct. Provide appropriate feedback.
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageResId = 0;
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        }
+        else {
+            messageResId = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
 }
